1cb1287971c3f7ffdfc46cd584542ff7b1a593b3
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-09-03 15:05:49 +0200

    Update podman-run-tinc.bash

--- podman-run-tinc.bash
+++ podman-run-tinc.bash
@@ -1,2 +1,4 @@
-podman build --no-cache --rm -f Containerfile -t tinc:demo .
-podman run --interactive --tty --cap-add=NET_ADMIN --device /dev/net/tun --security-opt="label=disable" tinc:demo
+mkdir -p "$(pwd)/tinc_config"
+
+podman build --no-cache --rm --volume $(pwd)/tinc_config/:/etc/tinc/ --build-arg VPN_NODE_DNS=name.dyndns.tld --file Containerfile --tag tinc:demo .
+podman run --interactive --tty --env VPN_NODE_DNS=name.dyndns.tld --publish 655:655 --volume $(pwd)/tinc_config/:/etc/tinc/ --cap-add=NET_ADMIN --device --security-opt="label=disable" /dev/net/tun tinc:demo
