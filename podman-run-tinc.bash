d40253ef704c76ae7714fb50a8a49395c4d94565
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-09-04 14:51:02 +0200

    Use variables for podman configuration
    Refactor podman commands to use variables for configuration.

--- podman-run-tinc.bash
+++ podman-run-tinc.bash
@@ -1,6 +1,17 @@
-mkdir -p "$(pwd)/tinc_config"
+# Configuration
+VPN_NODE_DNS="name.dyndns.tld"
+THIS_CLIENT_NAME="podman"
+THIS_CLIENT_ADDRESS="podman.dyndns.tld"
+VPN_NETWORK="yourtincvpnname"
+VPN_PEERS="remotetincname1 remotetincname2"
+VPN_SUBNET="10.11.21.0/24"
+THIS_CLIENT_UNIQUE_VPN_IP="10.11.21.42"
+TINC_CONFIG_DIR="$(pwd)/tinc_config"
+IMAGE_TAG="tinc:demo"
 
-podman build --no-cache --rm --volume $(pwd)/tinc_config/:/etc/tinc/ --build-arg VPN_NODE_DNS=name.dyndns.tld --file Containerfile --tag tinc:demo .
-#podman run --interactive --tty --env VPN_NODE_DNS=name.dyndns.tld --publish 655:655 --volume $(pwd)/tinc_config/:/etc/tinc/ --cap-add=NET_ADMIN --device /dev/net/tun --security-opt="label=disable" --security-opt=seccomp=unconfirmed --net=host tinc:demo
-#podman run --interactive --tty --env VPN_NODE_DNS=name.dyndns.tld --publish 655:655 --volume $(pwd)/tinc_config/:/etc/tinc/ --cap-add=NET_ADMIN --device /dev/net/tun --security-opt="label=disable" --security-opt=seccomp=unconfirmed tinc:demo
-podman run --interactive --tty --env VPN_NODE_DNS=name.dyndns.tld --publish 655:655 --volume $(pwd)/tinc_config/:/etc/tinc/ --cap-add=NET_ADMIN --device /dev/net/tun --security-opt="label=disable" tinc:demo
+mkdir -p "$TINC_CONFIG_DIR"
+
+podman build --no-cache --rm --volume "$TINC_CONFIG_DIR/":/etc/tinc/ --build-arg VPN_NODE_DNS="$VPN_NODE_DNS" --build-arg ThisClientName="$THIS_CLIENT_NAME" --build-arg ThisClientAddress="$THIS_CLIENT_ADDRESS" --build-arg VPN_NETWORK="$VPN_NETWORK" --build-arg VPNPeers="$VPN_PEERS" --build-arg VPNSubnet="$VPN_SUBNET" --build-arg ThisClientUniqueVpnIp="$THIS_CLIENT_UNIQUE_VPN_IP" --file Containerfile --tag "$IMAGE_TAG" .
+#podman run --interactive --tty --env VPN_NODE_DNS="$VPN_NODE_DNS" --env ThisClientName="$THIS_CLIENT_NAME" --env ThisClientAddress="$THIS_CLIENT_ADDRESS" --env VPNName="$VPN_NETWORK" --env VPNPeers="$VPN_PEERS" --env VPNSubnet="$VPN_SUBNET" --env ThisClientUniqueVpnIp="$THIS_CLIENT_UNIQUE_VPN_IP" --publish 655:655 --volume "$TINC_CONFIG_DIR/":/etc/tinc/ --cap-add=NET_ADMIN --device /dev/net/tun --security-opt="label=disable" --security-opt=seccomp=unconfirmed --net=host "$IMAGE_TAG"
+#podman run --interactive --tty --env VPN_NODE_DNS="$VPN_NODE_DNS" --env ThisClientName="$THIS_CLIENT_NAME" --env ThisClientAddress="$THIS_CLIENT_ADDRESS" --env VPNName="$VPN_NETWORK" --env VPNPeers="$VPN_PEERS" --env VPNSubnet="$VPN_SUBNET" --env ThisClientUniqueVpnIp="$THIS_CLIENT_UNIQUE_VPN_IP" --publish 655:655 --volume "$TINC_CONFIG_DIR/":/etc/tinc/ --cap-add=NET_ADMIN --device /dev/net/tun --security-opt="label=disable" --security-opt=seccomp=unconfirmed "$IMAGE_TAG"
+podman run --interactive --tty --env VPN_NODE_DNS="$VPN_NODE_DNS" --env ThisClientName="$THIS_CLIENT_NAME" --env ThisClientAddress="$THIS_CLIENT_ADDRESS" --env VPNName="$VPN_NETWORK" --env VPNPeers="$VPN_PEERS" --env VPNSubnet="$VPN_SUBNET" --env ThisClientUniqueVpnIp="$THIS_CLIENT_UNIQUE_VPN_IP" --publish 655:655 --volume "$TINC_CONFIG_DIR/":/etc/tinc/ --cap-add=NET_ADMIN --device /dev/net/tun --security-opt="label=disable" "$IMAGE_TAG"
